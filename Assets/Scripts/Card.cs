3cb579e8d1e4cad639f223df764b525e2dbc88ae
kjhwan <kjhwan0814@naver.com>
2024-09-09 19:44:31 +0900

    Revert "Merge branch 'dev' into feature-randomplace"
    This reverts commit c725dd268d491985967ed9d5594d9b5f0fde565c, reversing
    changes made to ec7753cffdc7f6923e878436b2817d253230c6d3.

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -4,29 +4,57 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    public GameObject front;
-    public GameObject back;
+    private bool _hasTransform = false;
 
-    public Animator anim;
+    private Transform _transform;
 
-
-
-
-    void Start()
-    {
-        
+    private Transform getTransform {
+        get
+        {
+            if(_hasTransform == false)
+            {
+                _hasTransform = true;
+                _transform = transform;
+            }
+            return _transform;
+        }
     }
 
-    
-    void Update()
+    public int index {
+        get;
+        private set;
+    }
+
+    public void Set(int index, Vector2 position, Sprite sprite)
     {
-        
+        this.index = index;
+        getTransform.position = position;
+        //frontImage.sprite = sprite; 
     }
 
     public void OpenCard()
     {
-        anim.SetBool("isOpen", true);
-        front.SetActive(true);
-        back.SetActive(false);
+    }
+
+    public void DestroyCard()
+    {
+        Invoke("DestroyCardInvoke", 0.5f);
+    }
+
+    void DestroyCardInvoke()
+    {
+        Destroy(gameObject);
+    }
+
+    public void CloseCard()
+    {
+        Invoke("CloseCardInvoke", 0.5f);
+    }
+
+    void CloseCardInvoke()
+    {
+        //animator.SetBool("isOpen", false);
+        //front.SetActive(false);
+        //back.SetActive(true);
     }
 }
