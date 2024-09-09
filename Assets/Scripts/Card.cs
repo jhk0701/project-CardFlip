a8442a1e0996faaa923a7a4505629340bc49277c
kjhwan <kjhwan0814@naver.com>
2024-09-09 19:49:20 +0900

    Fix: Conflic card

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -4,23 +4,38 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
+
     public GameObject front;
     public GameObject back;
 
     public Animator anim;
 
+    private bool _hasTransform = false;
 
+    private Transform _transform;
 
-
-    void Start()
-    {
-        
+    private Transform getTransform {
+        get
+        {
+            if(_hasTransform == false)
+            {
+                _hasTransform = true;
+                _transform = transform;
+            }
+            return _transform;
+        }
     }
 
-    
-    void Update()
+    public int index {
+        get;
+        private set;
+    }
+
+    public void Set(int index, Vector2 position, Sprite sprite)
     {
-        
+        this.index = index;
+        getTransform.position = position;
+        //frontImage.sprite = sprite; 
     }
 
     public void OpenCard()
@@ -29,4 +44,27 @@ public class Card : MonoBehaviour
         front.SetActive(true);
         back.SetActive(false);
     }
+
+    public void DestroyCard()
+    {
+        Invoke("DestroyCardInvoke", 0.5f);
+    }
+
+    void DestroyCardInvoke()
+    {
+        Destroy(gameObject);
+    }
+
+    public void CloseCard()
+    {
+        Invoke("CloseCardInvoke", 0.5f);
+    }
+
+    void CloseCardInvoke()
+    {
+        //animator.SetBool("isOpen", false);
+        //front.SetActive(false);
+        //back.SetActive(true);
+    }
+
 }
