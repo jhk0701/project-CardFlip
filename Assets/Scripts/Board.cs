104ac4281eba0442d663ecca29a1f3aef59cbb4a
jungyounghan <50984157+jungyounghan@users.noreply.github.com>
2024-09-10 19:56:35 +0900

    Refactor: Modify Board Code

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -7,22 +7,24 @@ public class Board : MonoBehaviour
     [SerializeField]
     private Card cardPrefab;
 
+    //[SerializeField]
+    //private Sprite junghwan1;
+    //[SerializeField]
+    //private Sprite junghwan2;
+    //[SerializeField]
+    //private Sprite younghan1;
+    //[SerializeField]
+    //private Sprite younghan2;
+    //[SerializeField]
+    //private Sprite chamsol1;
+    //[SerializeField]
+    //private Sprite chamsol2;
+    //[SerializeField]
+    //private Sprite jiyoon1;
+    //[SerializeField]
+    //private Sprite jiyoon2;
     [SerializeField]
-    private Sprite junghwan1;
-    [SerializeField]
-    private Sprite junghwan2;
-    [SerializeField]
-    private Sprite younghan1;
-    [SerializeField]
-    private Sprite younghan2;
-    [SerializeField]
-    private Sprite chamsol1;
-    [SerializeField]
-    private Sprite chamsol2;
-    [SerializeField]
-    private Sprite jiyoon1;
-    [SerializeField]
-    private Sprite jiyoon2;
+    private Sprite[] sprites;
 
     void Start()
     {
@@ -33,40 +35,41 @@ public class Board : MonoBehaviour
             Card card = Instantiate(cardPrefab, transform);
             float x = (i % 4) * 1.4f - 2.1f;
             float y = (i / 4) * 1.4f - 3.0f;
-            switch(arr[i]) {
-                case 0:
-                    //card.Set(0, new Vector2(x, y), junghwan1, CanOpenCard);
-                    card.Set(0, new Vector2(x, y), junghwan1);
-                    break;
-                case 1:
-                    //card.Set(1, new Vector2(x, y), junghwan2, CanOpenCard);
-                    card.Set(1, new Vector2(x, y), junghwan2);
-                    break;
-                case 2:
-                    //card.Set(2, new Vector2(x, y), younghan1, CanOpenCard);
-                    card.Set(2, new Vector2(x, y), younghan1);
-                    break;
-                case 3:
-                    //card.Set(3, new Vector2(x, y), younghan2, CanOpenCard);
-                    card.Set(3, new Vector2(x, y), younghan2);
-                    break;
-                case 4:
-                    //card.Set(4, new Vector2(x, y), chamsol1, CanOpenCard);
-                    card.Set(4, new Vector2(x, y), chamsol1);
-                    break;
-                case 5:
-                    //card.Set(5, new Vector2(x, y), chamsol2, CanOpenCard);
-                    card.Set(5, new Vector2(x, y), chamsol2);
-                    break;
-                case 6:
-                    //card.Set(6, new Vector2(x, y), jiyoon1, CanOpenCard);
-                    card.Set(6, new Vector2(x, y), jiyoon1);
-                    break;
-                case 7:
-                    //card.Set(7, new Vector2(x, y), jiyoon2, CanOpenCard);
-                    card.Set(7, new Vector2(x, y), jiyoon2);
-                    break;
-            }
+            card.Set(arr[i], new Vector2(x, y), sprites[arr[i]]);
+            //switch(arr[i]) {
+            //    case 0:
+            //        //card.Set(0, new Vector2(x, y), junghwan1, CanOpenCard);
+            //        card.Set(0, new Vector2(x, y), junghwan1);
+            //        break;
+            //    case 1:
+            //        //card.Set(1, new Vector2(x, y), junghwan2, CanOpenCard);
+            //        card.Set(1, new Vector2(x, y), junghwan2);
+            //        break;
+            //    case 2:
+            //        //card.Set(2, new Vector2(x, y), younghan1, CanOpenCard);
+            //        card.Set(2, new Vector2(x, y), younghan1);
+            //        break;
+            //    case 3:
+            //        //card.Set(3, new Vector2(x, y), younghan2, CanOpenCard);
+            //        card.Set(3, new Vector2(x, y), younghan2);
+            //        break;
+            //    case 4:
+            //        //card.Set(4, new Vector2(x, y), chamsol1, CanOpenCard);
+            //        card.Set(4, new Vector2(x, y), chamsol1);
+            //        break;
+            //    case 5:
+            //        //card.Set(5, new Vector2(x, y), chamsol2, CanOpenCard);
+            //        card.Set(5, new Vector2(x, y), chamsol2);
+            //        break;
+            //    case 6:
+            //        //card.Set(6, new Vector2(x, y), jiyoon1, CanOpenCard);
+            //        card.Set(6, new Vector2(x, y), jiyoon1);
+            //        break;
+            //    case 7:
+            //        //card.Set(7, new Vector2(x, y), jiyoon2, CanOpenCard);
+            //        card.Set(7, new Vector2(x, y), jiyoon2);
+            //        break;
+            //}
         }
     }
 
