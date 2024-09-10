1875cb916c93ce0472852342f74169349577548f
Real-pine <crewger9030@gmail.com>
2024-09-10 20:41:14 +0900

    feat:click_effect
    click particle effect

---
+++ Assets/Scripts/ClickEffect.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickEffect : MonoBehaviour
+{
+    public GameObject clickEffect;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            SpawnParticle();
+        }
+    }
+
+    void SpawnParticle()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            GameObject particle = Instantiate(clickEffect, hit.point, Quaternion.identity);
+            Destroy(particle, 1f);
+        }
+        else
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 10f;
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            GameObject particle = Instantiate(clickEffect, worldPosition, Quaternion.identity);
+            Destroy(particle, 1f);
+
+        }
+    }
+}
