bd129ce22714efb3a643b49736b50bba531159e5
Real-pine <crewger9030@gmail.com>
2024-09-11 10:24:13 +0900

    test: CloseCard time changed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -92,8 +92,8 @@ public class GameManager : MonoBehaviour
         }
         else{
             // not match
-            selectedCard.CloseCard(0.5f);
-            c.CloseCard(0.5f);
+            selectedCard.CloseCard(1f);
+            c.CloseCard(1f);
 
             ManagerSound.instance.StartSfx(ManagerSound.TypeSfx.Fail);
         }
